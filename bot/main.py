5a254ce5a5e564c45b17f9712fc8341fbc5a2abe
fatm1nd <semeritskiy@yandex.ru>
2023-06-24 21:41:02 +0300

    Update Dockerfile and docker-compose. Bot is successfully running.

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,24 @@
 import telebot
 import secret
 import os
+import sys
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
-load_dotenv(dotenv_path)
-botToken = os.environ.get("TELEGRAM_BOT_TOKEN")
+# load_dotenv(dotenv_path)
+# botToken = os.environ.get("TELEGRAM_BOT_TOKEN")
+
+botToken = sys.argv[1]
+# print(botToken)
 
 bot = telebot.TeleBot(token=botToken)
 
 
-
+@bot.message_handler(commands=['start'])
+def startFunction(message):
+    bot.send_message(message.chat.id,"Привет! Это бот для разделения пакетика чая с другими! А так же моно позвать кого-ниубдь на чаепитие или самому прийти!")
 
 
 @bot.message_handler(content_types=["location"])
